29dafe7272ef9c754c032136a0e0050a96d1c102
3hree6ix9ine <braithee@gmail.com>
2023-08-11 04:49:16 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,30 @@
 
 int main(void)
 {
-	char f;
+	int a, b;
 
-	f = 'a';
-	while
-		(f <= 'z') {
-			putchar(f);
+	a = '0';
+	b = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != '8' || (a == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a++;
 		}
+		a++;
+		b = '0';
+	}
 	putchar('\n');
 	return (0);
 }
