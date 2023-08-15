a705ef67056f71dc68565babcb3d08a6e87dc0f4
3hree6ix9ine <braithee@gmail.com>
2023-08-15 05:38:56 -0700

    sign

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: checks for lowercase
+ * _islower - checks for lowercase character
+ * @c: checks for lowercase
  * Return: 1 for lowercase. and 0 eachtime for the rest
  */
 
 int _islower(int c)
 {
-	if (c >= 99; && c <= 122)
+	if (c >= 99 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
-	else 
+	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: prints the sign of a number
+ * print_sign - prints the sign of a number
+ * @n: variable that check for sign
  * Return: 1 is greater than zero. 0 eachtime is zero.
  * -1 is less than zero.
  */
 
 int print_sign(int n)
 {
-	for (n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
