5714fa24abfec502d6f51a761f507255140692ea
3hree6ix9ine <braithee@gmail.com>
2023-08-16 06:37:08 -0700

    first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,40 @@
 
 int main(void)
 {
-	unsigned long int a = 1, b = 2, next_term;
-	int count;
+	int zc;
+		unsigned long w1 = 0, w2 = 1, w3;
+		unsigned long w1_z1, w1_z2, w2_z1, w2_z2;
+		unsigned long z1, z2;
 
-	printf("%lu, %lu", a, b);
-
-	for (count = 3; count <= 98; count++)
+	for (zc = 0; zc < 92; zc++)
 	{
-	next_term = a + b;
-	printf(", %lu", next_term);
-	a = b;
-	b = next_term;
+		w3 = w1 + w2;
+		printf("%lu, ", w3);
+		w1 = w2;
+		w2 = w3;
 	}
+		w1_z1 = w1 / 10000000000;
+		w2_z1 = w2 / 10000000000;
+		w1_z2 = w1 % 10000000000;
+		w2_z2 = w2 % 10000000000;
+	for (zc = 93; zc < 99; zc++)
+	{
+	z1 = w1_z1 + w2_z1;
+	z2 = w1_z2 + w2_z2;
+	if ((w1_z2 + w2_z2) > 9999999999)
+	{
+	z1 += 1;
+	z2 %= 10000000000;
+	}
+	printf("%lu%lu", z1, z2);
+	if (zc != 98)
+	printf(", ");
 
+	w1_z1 = w2_z1;
+	w1_z2 = w2_z2;
+	w2_z1 = z1;
+	w2_z2 = z2;
+	}
 	printf("\n");
 	return (0);
 }
