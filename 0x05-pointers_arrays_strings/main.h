e40c36682a3f7370f01b7716809956750caded92
3hree6ix9ine <braithee@gmail.com>
2023-08-21 04:04:09 -0700

    I do not fear computers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -15,5 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
