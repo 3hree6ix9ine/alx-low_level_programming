a7b54cbafbe6801df8b4822525b74af178fdfbcc
3hree6ix9ine <braithee@gmail.com>
2023-09-13 16:38:46 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include "3-calc.h"
+
 
 /**
  * op_add - returns the sum of a and b
@@ -62,10 +61,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
